cf3557534d632fc0ac6e8a99105095f995a85902
Giuliano Sposito <gsposito@ciandt.com>
2018-10-19 11:24:19 -0300

    análise de distribuição dos erros de projeção

--- R/_draft/pontuation_analysis.R
+++ R/_draft/pontuation_analysis.R
@@ -1,16 +1,19 @@
 library(tidyverse)
 
+# carregando pontos e projeções
 points <- readRDS("./data/players_points.rds")
 projections <- readRDS("./data/players_projections.rds") %>% 
   rename(pts.proj = points)
 
+# montando dataset de comparação pontos e projecao
 points %>% 
   inner_join(projections, by = c("src_id", "season", "week")) %>%
   select(id, player, position, season, week, points, data_src, pts.proj) %>% 
   mutate( data_src = as.factor(data_src),
-          position = as.factor(position) ) -> player.data
+          position = as.factor(position),
+          error = (pts.proj-points) ) -> player.data
 
-
+# Plotando a projeção contra a pontuação
 player.data %>% 
   ggplot(aes(x=points, y=pts.proj)) +
   geom_point(aes(color=position)) +
@@ -19,13 +22,38 @@ player.data %>%
   theme_bw() +
   theme( legend.position = "none" )
 
-
+# Erro é constante alo longo dos pontos projetados?
 player.data %>% 
-  mutate( error = (pts.proj-points) ) %>% 
+  # filter(data_src=="CBS",position=="WR", !is.na(error)) %>% 
+  ggplot(aes(x=pts.proj, y=error)) +
+  geom_point(aes(color=position)) +
+  facet_grid( position~data_src ) +
+  theme_bw() +
+  theme( legend.position = "none" )
+
+# distribuição de erro por posicao e site
+player.data %>% 
+  filter( !(points==0 & pts.proj==0) ) %>% 
   ggplot() +
   geom_histogram(aes(error, fill=position)) +
   facet_grid( position~data_src ) +
   theme_bw() +
   theme( legend.position = "none" )
-  
-  
+
+# pegando um dos casos Wide Receiver na CBS
+player.data %>% 
+  filter(data_src=="CBS",position=="WR", !is.na(error)) %>% 
+  ggplot() +
+  geom_histogram(aes(error, fill=position)) +
+  theme_bw() +
+  theme( legend.position = "none" )
+
+# comparando com uma curva amostrada
+player.data %>% 
+  filter(data_src=="CBS",position=="WR", !is.na(error)) %>% 
+  pull(error) %>% 
+  sample(size=100, replace = T) %>% 
+  hist(col="red", breaks = 50)
+
+# quantos pontos ha por site e posicao ?
+table(player.data$position, player.data$data_src)
