c24e56a8f2f860d3edb87830cb8209f26cdf7ceb
Giuliano Sposito <gsposito@ciandt.com>
2018-11-28 00:40:21 -0200

    ajuste do pipe para semana 13 bugfix para semana 13

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -7,8 +7,8 @@ source("./R/import/checkFantasyAPI.R")
 source("./R/import/import_matchups.R")
 source("./R/simulation/points_simulation_v3.R")
 
-week <- 12
-prefix <- "posSNF"
+week <- 13
+prefix <- "preFA"
 
 checkFantasyAPI(week)
 
@@ -19,7 +19,7 @@ simulateGames(week) -> sim
 
 rmarkdown::render(
   input = "./R/reports/dudes_simulation_v2.Rmd",
-  output_file = glue("../../public/dudes_simulation_week12_{prefix}.html"),
+  output_file = glue("../../public/dudes_simulation_week{week}_{prefix}.html"),
   output_format = "flex_dashboard"
   )
 
