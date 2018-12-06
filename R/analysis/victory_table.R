325876d2df067ffaf8cc329fa0f97ced7ffdfaed
Giuliano Sposito <gsposito@ciandt.com>
2018-12-06 01:05:57 -0200

    classificacao semanal

--- R/analysis/victory_table.R
+++ R/analysis/victory_table.R
@@ -49,21 +49,46 @@ rounds %>%
 tibble(
   week = as.integer(c(gm.pts$week,      gm.pts$week)),
   team = c(gm.pts$home.name, gm.pts$away.name),
-  pts  = c(gm.pts$home.pts,  gm.pts$away.pts)
+  pts  = c(gm.pts$home.pts,  gm.pts$away.pts),
+  win = c(gm.pts$home.win, gm.pts$away.win),
+  loss = c(gm.pts$away.win, gm.pts$home.win)
 ) %>% 
   group_by(week) %>% 
-  mutate(rank = rank(pts)) %>% 
+  mutate(pts.rank = rank(desc(pts))) %>% 
   ungroup() %>% 
-  arrange(week, rank) -> team.points
+  arrange(week, pts.rank) %>% 
+  group_by(team) %>% 
+  mutate(
+    points = cumsum(pts),
+    wins   = cumsum(win),
+    losses = cumsum(loss)
+  ) %>% 
+  ungroup() %>% 
+  group_by(week) %>% 
+  mutate(win.rank = rank(desc((wins-losses)*10000+points))) %>% 
+  ungroup() %>% 
+  arrange(week, win.rank) -> team.points
 
 library(plotly)
 
 team.points %>% 
-  ggplot( aes(x=week, y=rank, color=team) ) +
+  ggplot( aes(x=week, y=win.rank, color=team) ) +
   geom_line(size=2) +
-  geom_point(aes(label=pts), size=4, label.padding = unit(2, "points"), show.legend = F) +
-  theme_minimal() +
-  theme( legend.position = "bottom" ) -> g
+  geom_label(aes(label=paste(wins, losses, sep="-")), size=4, label.padding = unit(2, "points"), show.legend = F) +
+  theme_void() +
+  theme( legend.position = "bottom" ) +
+  theme(panel.grid.major.y = element_blank()) +
+  scale_y_reverse() +
+  geom_text(data=filter(team.points, week==1), aes(label=team, x=-1.5, color=team) , hjust = -.05, size = 3.5, fontface=2, show.legend=F) +
+  geom_text(data=filter(team.points, week==13), aes(label=team, x=15.5, color=team) , hjust = 1.05, size = 3.5, fontface=2, show.legend=F)
+
+
+
+geom_text(data = df.world_ports %>% filter(year == "2014", rank <= param.rank_n), aes(label = port_label, x = '2014') , hjust = -.05, color = "#888888", size = 4) +
+  geom_text(data = df.world_ports %>% filter(year == "2004", rank <= param.rank_n), aes(label = port_label, x = '2004') , hjust = 1.05, color = "#888888", size = 4) +
+  
+
+
 
 saveRDS(g, "./data/ggplot_points_rank.rds")
 
