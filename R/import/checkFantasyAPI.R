3890838ed6ef637a7476c324c44874ab3fa63a56
Giuliano Sposito <gsposito@ciandt.com>
2019-09-08 12:14:02 -0300

    ajustes de checkagem da api

--- R/import/checkFantasyAPI.R
+++ R/import/checkFantasyAPI.R
@@ -4,7 +4,7 @@ library(httr)
 library(jsonlite)
 library(glue)
 
-checkFantasyAPI <- function(week){
+checkFantasyAPI <- function(.week){
   
   # lendo liga e token do yaml (para n?o versionar o access token)
   config <- yaml.load_file("./config/config.yml")
@@ -15,7 +15,7 @@ checkFantasyAPI <- function(week){
   url.matchups <- "http://api.fantasy.nfl.com/v1/league/matchups?leagueId={leagueId}&week={week}&format=json&authToken={authToken}"
   
   # faz a chamada na api
-  httr::GET(glue(url.matchups)) -> resp
+  resp <- httr::GET(glue(url.matchups, week=.week))
   
   return(resp$status_code==200)
   
