3c39628dcb3086ccef7efd5c08fbd599dbf971d7
Giuliano Sposito <gsposito@ciandt.com>
2019-09-12 14:01:38 -0300

    Tentativas de incluir o Slye

--- R/_draft/k_slye_id_finder.R
+++ R/_draft/k_slye_id_finder.R
@@ -1,17 +1,36 @@
+library(tidyverse)
+scraps <- readRDS("./data/week2_scrap.rds")
+
 # SCRAP
-scraps[["QB"]] %>% 
-  filter(team=="ARI", data_src=="NumberFire") %>%
+scraps[["K"]] %>% 
+  filter(team=="CAR", data_src=="NumberFire") %>%
   mutate(id=as.integer(id)) %>%
   glimpse()
 
 # PLAYER ID 
+load("../ffanalytics/R/sysdata.rda") # <<- Players IDs !!!
+player_ids <- player_ids %>% 
+  mutate(id=as.integer(id),
+         nfl_id=as.integer(nfl_id)) %>% 
+  mutate(nfl_id = ifelse(id==14600,2563132, nfl_id)) %>% 
+  as_tibble()
+
 player_ids %>%
-  filter(numfire_id=="kyler-murray") %>% 
+  filter(numfire_id=="joey-slye") %>% 
   glimpse()
   
 # FANTASY
+matchups <- readRDS("./data/week2_matchups_json.rds")
+
 x <- matchups %>% 
   map(~as_tibble(.x[[1]]))
-x[[2]]$matchup$awayTeam$players[[1]] %>% 
-  filter(pos)
+
+x[[1]]$matchup$awayTeam$players[[1]] %>% 
+  filter(position=="K")  %>% 
+  glimpse()
+
+
+### Keys Sequence ###
+#
+# Scrap (id) -> (id) Players_id (nfl_id) -> Fantasy(id)
 
