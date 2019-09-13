bd17809188290adb0a49c49271ae99db97413892
Giuliano Sposito <gsposito@ciandt.com>
2019-09-13 00:13:08 -0300

    Plotando erros e projecao de pontuacao dos jogadores

--- R/_draft/players_error.R
+++ R/_draft/players_error.R
@@ -8,18 +8,28 @@ pts  <- readRDS("./data/players_points.rds")
 cur.week <- 2
 
 dtf <- pts %>% 
-  rename(nfl_id=id.y) %>% 
-  inner_join(proj, by=c("nfl_id"="id", "week", "season"))  %>% 
+  #rename(nfl_id=id.y) %>% 
+  inner_join(proj, by=c("id"="id", "week", "season"))  %>% 
   select(id, nfl_id, season, week, pos, data_src, pts.proj, points )  %>% 
   filter(week!=cur.week) %>% 
-  mutate(error=pts.proj-points)
+  mutate(error=points-pts.proj)
 
 dtf %>% 
   ggplot() +
   geom_point(aes(x=pts.proj, y=points, color=pos)) +
   facet_grid(pos~data_src, scales = "free") +
   geom_abline(intercept=0, slope=1, linetype="dashed") + 
+  ggtitle("Week 1 - Projection", subtitle = "By Position and Source Site") +
+  xlim(0,30) + ylim(0,30)
+
+dtf %>% 
+  ggplot() +
+  geom_density(aes(x=error, fill=pos)) +
+  facet_grid(pos~data_src, scales = "free") +
+  geom_vline(xintercept = 0, color="black", linetype="dashed") +
+  ggtitle("Week 1 - Error Distribution", subtitle = "By Position and Source Site") +
   theme_minimal()
+
 
 dtf %>% 
   group_by(data_src, pos) %>% 
