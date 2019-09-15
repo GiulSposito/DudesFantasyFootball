b226ab8c19950eea2a7150596d31de21674083d5
Giuliano Sposito <gsposito@ciandt.com>
2019-09-15 14:52:42 -0300

    Limpeza de codigo antigo

--- R/tidy/matchups.R
+++ R/tidy/matchups.R
@@ -12,19 +12,6 @@ nfl2ffa <- function(.dtf, .ids=.players_id) {
     return()
 }
 
-# funcao para extrar dados dos hosters dos times
-# .extractTeamRoster <- . %>% 
-#   .$players %>% 
-#   .[[1]] %>% 
-#   select( src_id=id, name, position, rosterSlot, fantasyPts ) %>%
-#   jsonlite::flatten() %>% 
-#   select(-fantasyPts.week.season, -fantasyPts.week.week ) %>% 
-#   rename(points = fantasyPts.week.pts) %>% 
-#   mutate(
-#     src_id = as.integer(src_id),
-#     points = as.numeric(points)
-#   )
-
 # precessa o json de retorno
 
 extractTeams <- function(.matchup.teams.json) {
@@ -60,21 +47,5 @@ extractTeams <- function(.matchup.teams.json) {
     }) %>%
     bind_rows() %>% 
     return()
-  
-
-      # matchup <- .json$leagues$matchup
-      # tibble(
-      #   home.teamId = matchup$homeTeam$id,
-      #   home.name   = matchup$homeTeam$name,
-      #   home.logo   = matchup$homeTeam$logoUrl,
-      #   home.pts    = as.numeric(matchup$homeTeam$pts),
-      #   home.roster = list(.extractTeamRoster(matchup$homeTeam)),
-      #   away.teamId = matchup$awayTeam$id,
-      #   away.name   = matchup$awayTeam$name,
-      #   away.logo   = matchup$awayTeam$logoUrl,
-      #   away.pts    = as.numeric(matchup$awayTeam$pts),
-      #   away.roster = list(.extractTeamRoster(matchup$awayTeam))
-      # ) %>% 
-      #  return()
 }
 
