0b6995bb742dce5de596470bdec606bb0761d396
Giuliano Sposito <gsposito@ciandt.com>
2019-09-24 11:36:21 -0300

    revisando grafico de pontuacao prevista para mostrar todos os times

--- R/_draft/waivers_starts.R
+++ R/_draft/waivers_starts.R
@@ -3,24 +3,12 @@ library(tidyverse)
 players <- readRDS("./data/week4_players_projections.rds") %>% 
   filter(
     fantasy.team %in% c("*FreeAgent","Bikers"),
-    !(team %in% c("FA", "FA*"))
+    !(team %in% c("FA", "FA*","NYJ","SFO"))
   ) 
-
-# barkley 13604
-players %>% 
-  filter(team=="NYG", position=="RB")
-
-# barkley 13604
-# Gould 8153
-# SF Def 530
-players %>% 
-  filter(team=="SFO", position=="DST")
-
-
 
 # starts
 starters <- tibble(
-  pos=c("QB","WR","RB","TE","K","DST"),
+  pos=c("QB","RB","WR","TE","K","DST"),
   qtd=c(1,2,2,1,1,1)
 ) %>% 
   split(1:nrow(.)) %>% 
@@ -38,17 +26,25 @@ starters <- players %>%
   top_n(1, floor) %>% 
   bind_rows(starters,.)
 
-starters 
-
 ## bench
-tibble(
-    pos=c("K","QB","WR","RB","TE"),
-    qtd=c(1,1,2,2,1)
+bench <- tibble(
+    pos=c("QB","RB","WR","TE","K"),
+    qtd=c(1,1,2,1,1)
   ) %>% 
   split(1:nrow(.)) %>% 
   map_df(function(.x, .players){
     .players %>% 
-      # filter(position==.x$pos, team !="MIA") %>% 
-      top_n(.x$qtd, ceiling)
+      filter(position==.x$pos) %>% 
+      top_n(.x$qtd, floor)
   }, .players = anti_join(players, starters) )
+
+# releases
+releases <- players %>% 
+  filter(fantasy.team=="Bikers") %>% 
+  anti_join(starters) %>% 
+  anti_join(bench)
+
+starters
+bench 
+releases
 
