b60656a0d1d407ba66f13ecf314d324c7296ae36
Giuliano Sposito <gsposito@ciandt.com>
2019-09-27 15:05:03 -0300

    Incorporando importacao de status do game (se jogador já jogou?)

--- R/import/import_matchups.R
+++ R/import/import_matchups.R
@@ -4,6 +4,8 @@ library(httr)
 library(jsonlite)
 library(glue)
 
+source("./R/api/ffa_league.R")
+
 importMatchups <- function(.week, .saveToFile=T){
   
   # lendo liga e token do yaml (para n?o versionar o access token)
@@ -43,3 +45,36 @@ importMatchups <- function(.week, .saveToFile=T){
   return(matchup.teams.json)  
 }
 
+# busca na nova api de rosters o status de game do jogador (se ele já jogou)
+importPlayerGameStatus <- function(.week, .player_ids){
+  
+  #configuracoes de acesso
+  .config    <- yaml.load_file("./config/config.yml")
+  .leagueId  <- .config$leagueId
+  .authToken <- .config$authToken
+  
+  # chama a API
+  resp <- ffa_league_matchups(.authToken, .leagueId, .week)
+
+  # obtem a relacao de times
+  teams <- resp$content$games$`102019`$leagues[[.leagueId]]$teams
+  
+  # percorre os rosters indo atras da informacao sobre "Editavel"
+  playerGameStatus <- map_df(teams, function(.team){
+      .team$rosters[[1]] %>% 
+        bind_rows() %>%
+        return()
+    }, .id="teamId") %>% 
+    as_tibble() %>% 
+    mutate_at(vars(teamId, rosterSlotId, playerId), as.integer) %>% 
+    mutate( played = !isEditable ) %>%  # cria uma flag para saber se o jogador 
+    select(teamId, playerId, played) %>% 
+    return()
+  
+  # casa os IDs e retorna
+  .player_ids %>% 
+    select(id, nfl_id) %>%
+    inner_join(playerGameStatus, by=c("nfl_id"="playerId")) %>% 
+    return()
+  
+}
