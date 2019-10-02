d0a63c9eb841bae8395a6f9a9340a9a3493ffeee
Giuliano Sposito <gsposito@ciandt.com>
2019-10-02 19:06:17 -0300

    Draft para fazer a evolução do ranking

--- R/_draft/league_weekly_rank.R
+++ R/_draft/league_weekly_rank.R
@@ -19,7 +19,8 @@ matchups <- weeks %>%
     .saveToFile = F
   )
 
-saveRDS(matchups, "./data/weekly_matchups.rds")
+saveRDS(matchups, "./data/post_matchups_results.rds")
+matchups <- readRDS("./data/post_matchups_results.rds")
 
 # para cada time do roster tira informacoes de ponto, se ganhou e qual o rank
 extractTeam <- . %>% 
@@ -74,19 +75,38 @@ wrank <- matchups %>%
     extractWeekGames,
     .id="week"
   ) # %>% 
-  # mutate( id=factor(id), team=factor(team) )
 
 # cria a tabela (long) de confrontos diretos
-h2h.table <- wrank %>% 
+# h2h.table <- wrank %>% 
+#   select(week, id, team, outcome, op.out, op.team, op.id) %>% 
+#   mutate( h2h.balance = (outcome=="win") - (op.out=="win") ) %>% 
+#   group_by(id, team, op.id, op.team) %>% 
+#   summarise( balance = sum(h2h.balance)) %>% 
+#   ungroup()
+
+# para a geracao no confronto direto
+# por semana computa o h2h balance time a time
+h2h.weekly <-  wrank %>% 
   select(week, id, team, outcome, op.out, op.team, op.id) %>% 
-  mutate( h2h.balance = (outcome=="win") - (op.out=="win") ) %>% 
-  group_by(id, team, op.id, op.team) %>% 
-  summarise( balance = sum(h2h.balance)) %>% 
-  ungroup()
+  mutate( h2h.balance = (outcome=="win") - (op.out=="win") )
 
-# salva dados
-# saveRDS(wrank, "./data/weekly_results.rds")
+# entao monta uma tabela acumulando semana a semana
+# exemplo, na semana 3, na verdade tem a soma do h2h da semana 1, 2 e 3
+h2h.table <-  1:4 %>% # quantas semanas?
+  map(~seq(1,.x)) %>% # gera uma lista de semanas (1, 1:2, 1:3, 1:4, ...)
+  map_df(function(.weeks, .h2hw){
+    .h2hw %>% 
+      filter(week %in% .weeks) %>% # vai acumulando 
+      group_by(id, team, op.id, op.team) %>% 
+      summarise( balance = sum(h2h.balance)) %>% 
+      ungroup() %>% 
+      mutate(week = max(.weeks)) %>% 
+      return()
+  }, 
+  .h2hw = h2h.weekly)
 
+# computa time time, semana a semana o 
+# numero de vitorias, terrotas, pontos pro e contra
 wrank <- wrank %>% 
   group_by(team) %>% 
   arrange(team,week) %>% 
@@ -98,18 +118,34 @@ wrank <- wrank %>%
   ) %>% 
   ungroup()
 
-wrank <- wrank %>% 
+# agrupa, semana a semana os grupos de time com mesmo
+# numero de vitorias e derrotas
+finalrank <- wrank %>% 
+  mutate( .week = week ) %>% # precisa aninhar a semana para passar para a h2h.table dentro do grupo
   group_by(week, wins, losses) %>% 
   nest() %>% 
   mutate(
     data = map(data, function(.x, .h2ht){
+      
+      # descobre a semana do grupo
+      sel.week <- unique(.x$.week)
+      
+      # filtra qual eh a h2h table que importa
+      wh2h <- filter(.h2ht, week==sel.week)
+      
+      # gera as comubinações entre todos os oponentes do grupo
       expand.grid(id=.x$id, op.id=.x$id) %>% 
-        inner_join(.h2ht, by = c("id", "op.id")) %>% 
+        # junta com o balanco h2h
+        inner_join(wh2h, by = c("id", "op.id")) %>% 
         group_by(id, team) %>% 
+        # calcula o balanco entre os times do grupo
         summarise(h2h.balance=sum(balance)) %>% 
         ungroup() %>% 
+        # recompoem com dados do resultado da semana
         right_join(.x, by = c("id", "team")) %>% 
+        # zera o balanco se houver NA
         mutate( h2h.balance = ifelse(is.na(h2h.balance),0,h2h.balance) ) %>% 
+        select(-.week) %>% 
         return()
     },
     .h2ht=h2h.table)
@@ -119,14 +155,14 @@ wrank <- wrank %>%
   mutate( wrank = rep(1:10,4) ) %>% 
   select(-op.id, -op.team, -op.out)
   
-wrank %>%
+finalrank %>%
   ggplot(aes(x=week, y=pts, group=team)) +
   geom_line(aes(color=team), size=1) +
   geom_image(aes(image=logoUrl)) +
   ggtitle("Pontuação dos Times", "por semana") +
   theme_minimal()
 
-wrank %>% 
+finalrank %>% 
   ggplot(aes(x=week, y=pts.pro, group=team)) +
   geom_line(aes(color=team), size=1) +
   geom_image(aes(image=logoUrl), size=.03) +
@@ -134,11 +170,13 @@ wrank %>%
   theme_minimal()
 
 
-wrank %>% 
+finalrank %>% 
+  mutate(wlt=paste0(wins, "-", losses)) %>% 
   ggplot(aes(x=week, y=reorder(wrank,-wrank), group=team)) +
   geom_line(aes(color=team), size=2) +
   geom_image(aes(image=logoUrl), size=.04) +
   geom_hline(yintercept = 6.5, linetype="dashed", size=1, color="darkgrey") +
+  geom_text(aes(label=wlt), size=2.5, color="black", nudge_y = -.33) +
   geom_text(x=4.4, y=6.65, label="playoff clinch", size=3, color="darkgrey") +
   ylab("rank") +
   ggtitle("Weekly Ranking") +
