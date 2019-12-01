746f43ff9e4b4733f1d91ae1741dd1e833eb2cb9
Giuliano Sposito <gsposito@ciandt.com>
2019-12-01 18:11:38 -0300

    after 1st sunday games

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -6,7 +6,7 @@ library(glue)
 # parametros de execucao
 week <- 13
 sim.version <- 4
-prefix <- "preSundayGames"
+prefix <- "pos1stSundayGames"
 destPath <- "static"
 
 source("./R/import/checkFantasyAPI.R")
