e0db59cdf2877b3512fd82d6fef102ff8636798c
Giuliano Sposito <gsposito@ciandt.com>
2019-12-12 19:47:42 -0300

    pre TNF w FFToday

--- R/_draft/nfl_comperank.R
+++ R/_draft/nfl_comperank.R
@@ -1,59 +1,43 @@
-install.packages("comperank")
+# install.packages("comperank")
+library(comperes)
 library(tidyverse)
+library(comperank)
 
 gm_res <- readRDS("./data/post_matchups_results.rds")
 
-# para cada time do roster tira informacoes de ponto, se ganhou e qual o rank
-extractTeam <- . %>% 
-  select(id, team=name, logoUrl, pts, outcome) %>% 
-  mutate(
-    id = as.integer(id),
-    win=(outcome=="win"),
-    loss=(outcome!="win"),
-    pts=as.numeric(pts)
-  ) %>% 
-  as_tibble()
-
-# adiciona informacoes do oponente para fazer o ranking H2H
-addOpponent <- function(.t, .o){ 
-  .t %>% 
-    mutate(
-      pts.ctr = .o$pts,
-      op.id   = .o$id,
-      op.team = .o$team,
-      op.out  = .o$outcome
-    ) %>% 
-    return()
+getTeam <- function(.team, .prefix){
+  .team %>% 
+    select(id, name, pts) %>% 
+    mutate(id = as.integer(id), pts=as.numeric(pts)) %>% 
+    set_names(paste0(.prefix,".",names(.)))
 }
 
-# para cada jogo dentro da semana
-# processa os times
-extractGame <- function(.game) {
-  
-  .teams <- .game[[1]]$matchup 
-  
-  at <- extractTeam(.teams$awayTeam)
-  ht <- extractTeam(.teams$homeTeam)
-  
-  at <- addOpponent(at, ht)
-  ht <- addOpponent(ht, at)
-  
-  bind_rows(at,ht) %>% 
-    return()
-  
-}
-
-# para cada conjunto de jogos dentre de uma semana
-# processa o jogo
-extractWeekGames <- function(.weekgames){
-  .weekgames %>% 
-    map_df(extractGame)
-}
-
-# para todas as semanas
-wrank <- matchups %>% 
+games <- gm_res %>% 
   map_df(
-    extractWeekGames,
+    function(.weekgames){
+      map_df(.weekgames,
+             function(.game)
+               bind_cols(
+                 getTeam(.game$leagues$matchup$awayTeam, "away"),
+                 getTeam(.game$leagues$matchup$homeTeam, "home")
+               ),
+             .id="week.game.id"
+      )
+    },
     .id="week"
   ) %>% 
-  mutate(week=as.integer(week))
+  mutate(game.id = as.integer(paste0(week, week.game.id))) %>% 
+  select(-week, -week.game.id) %>% 
+  as_tibble()
+
+games.long <- bind_rows(
+  select(games, game=game.id, player=home.name, score=home.pts),
+  select(games, game=game.id, player=away.name, score=away.pts)
+) 
+
+games.long %>% 
+  h2h_long()
+
+games.long %>% 
+  rank_colley(keep_rating = T) %>% 
+  arrange(ranking_colley)
