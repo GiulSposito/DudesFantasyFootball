3dd92bff402eff54b8d6ca5454bfa7b5e7d53133
Giuliano Sposito <gsposito@ciandt.com>
2020-08-18 08:48:56 -0300

    Removendo API V1 Modificando parametros para pegar estatisticas de pontuacao

--- R/api/ffa_players.R
+++ R/api/ffa_players.R
@@ -1,9 +1,11 @@
+library(glue)
 source("./R/api/ffa_api.R")
 
-# return the user id, leagues and teams of a 'authToken'
-ffa_players_all <- function(.authToken, .leagueId){
 
-  players <- ffa_api2(
+# return the league players
+ffa_players <- function(.authToken, .leagueId){
+  
+  players <- ffa_api(
     .path = "v2/league/players",
     .query = list(
       "appKey"    = "internalemailuse",
@@ -14,15 +16,37 @@ ffa_players_all <- function(.authToken, .leagueId){
   
 }
 
-ffa_players_resp_to_tibble <- function(.resp) {
-  .resp$content$games$`102019`$players %>%
-    map_dfr(function(.player){
-      .player %>% 
-        map(~ ifelse(is.null(.x), NA, .x)) %>% 
-        as_tibble() %>% 
-        return()
-    }) %>% 
-    mutate_at(vars(playerId, nflTeamId, byeWeek), as.integer) %>% 
-    mutate_at(vars(esbId, position, nflTeamAbbr, injuryGameStatus), factor) %>% 
-    return()
+# return the league player with stats
+ffa_players_stats <- function(.authToken, .leagueId, .season, .weeks){
+
+  stats_str <- .weeks %>% 
+    map_chr(~glue('{"type":"stats","season":"<<.season>>","week":"<<.x>>"}', .open = "<<", .close = ">>")) %>% 
+    c(.,glue('{"type":"stats","season":"<<.season>>"}', .open = "<<", .close = ">>")) %>% 
+    paste(collapse = ",") %>% 
+    paste0("[",.,"]")
+  
+    
+  players <- ffa_api(
+    .path = "v2/league/players",
+    .query = list(
+      "appKey"    = "internalemailuse",
+      "authToken" = .authToken,
+      "leagueId"  = .leagueId,
+      "stats"     = stats_str,
+      "count"     = 2000
+    ))
+  
 }
+
+
+# # stats reference
+# 
+# [
+#   {"type":"researchStats","season":"2015","week":"1"},
+#   {"type":"ranks","season":"2015","week":"1"},
+#   {"type":"stats","season":"2015","week":"1"},
+#   {"type":"stats","season":"2015"},
+#   {"type":"projectedStats","season":"2015","week":"1"},
+#   {"type":"nflGames","season":"2015","week":"1"}
+# ]
+
