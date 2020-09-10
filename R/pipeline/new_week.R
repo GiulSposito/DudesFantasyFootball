4e1b65feee426b4ebee0ea3235aa68f23cb9a377
Giuliano Sposito <gsposito@ciandt.com>
2020-09-10 20:17:17 -0300

    pipe avancando ate a simulacao novo codigo (v5) da simulacao

--- R/pipeline/new_week.R
+++ R/pipeline/new_week.R
@@ -11,7 +11,7 @@ season <- 2020
 config <- read_yaml("./config/config.yml")
 prefix <- "preTNF"
 destPath <- "static/reports/2020"
-sim.version <- 3
+sim.version <- 5
 
 # API ACCESS CHECK ####
 source("./R/import/checkFantasyAPI.R")
@@ -137,80 +137,83 @@ rmarkdown::render(
 source("./R/simulation/players_projections.R")
 ptsproj <- calcPointsProjection(season, read_yaml("./config/score_settings.yml"))
 
-# simula as partidas
+# simulação das partidas
 source(glue("./R/simulation/points_simulation_v{sim.version}.R"))
-sim <- simulateGames(week)
+sim <- simulateGames(week, season, ptsproj, matchups_games, teams_rosters, players_stats, player_ids)
 
-
-##### import matchups
-# source("./R/import/import_matchups.R")
-# matchups <- importMatchups(week, .saveToFile = F)
-# saveRDS(matchups, glue("./data/week{week}_matchups_json.rds"))
-# source("./R/api/ffa_league.R")
-# leagueMatchups <- ffa_league_matchups(config$authToken, config$leagueId, week)
-# matchups_games <- ffa_extractMatchups(leagueMatchups)
-# teams_rosters  <- ffa_extractTeams(leagueMatchups)   
-
-##### import player statistics
-# source("./R/import/import_player_stats.R")
-# player_stats <- importPlayerStatistics(1:week, .saveToFile = F)
-# saveRDS(player_stats, "./data/players_points.rds")
-# source("./R/api/ffa_players.R")
-# player_stats <- ffa_players_stats(config$authToken, config$leagueId, season, 1:week) %>% 
-#   ffa_extractPlayersStats()
-
-##### import predicions, calc projections and add Teams
-# source("./R/import/ffa_player_projection.R")
-# scraps <- scrapPlayersPredictions(week, season, F)
-# projs  <- calcPlayersProjections(scraps, read_yaml("./config/score_settings.yml"))
-
-# tipos de status que zera a pontuacao
-# injuryStatus <- c("Suspended","Injured Reserve","Not With Team")
-
-# cola informacao de times as projecoes de pontos dos jogadores
-# projs.team <- projs %>% 
-#   inner_join(mutate(player_ids, id=as.integer(id)), by="id") %>% 
-#   addTeams(matchups, week, F) %>% 
-#   ## Zera Estatisticas de quem estah machucado
-#   mutate(
-#     points  = ifelse(injuryGameStatus %in% injuryStatus, 0, points),
-#     floor   = ifelse(injuryGameStatus %in% injuryStatus, 0, floor),
-#     ceiling = ifelse(injuryGameStatus %in% injuryStatus, 0, ceiling),
-#     sd_pts  = ifelse(injuryGameStatus %in% injuryStatus, 0, sd_pts),
-#   )
-
-## projection report
+# salva resultado
+saveRDS(sim, glue("./data/simulation_v{sim.version}_week{week}_{prefix}.rds"))
+ 
+ 
+# ##### import matchups
+# # source("./R/import/import_matchups.R")
+# # matchups <- importMatchups(week, .saveToFile = F)
+# # saveRDS(matchups, glue("./data/week{week}_matchups_json.rds"))
+# # source("./R/api/ffa_league.R")
+# # leagueMatchups <- ffa_league_matchups(config$authToken, config$leagueId, week)
+# # matchups_games <- ffa_extractMatchups(leagueMatchups)
+# # teams_rosters  <- ffa_extractTeams(leagueMatchups)   
+# 
+# ##### import player statistics
+# # source("./R/import/import_player_stats.R")
+# # player_stats <- importPlayerStatistics(1:week, .saveToFile = F)
+# # saveRDS(player_stats, "./data/players_points.rds")
+# # source("./R/api/ffa_players.R")
+# # player_stats <- ffa_players_stats(config$authToken, config$leagueId, season, 1:week) %>% 
+# #   ffa_extractPlayersStats()
+# 
+# ##### import predicions, calc projections and add Teams
+# # source("./R/import/ffa_player_projection.R")
+# # scraps <- scrapPlayersPredictions(week, season, F)
+# # projs  <- calcPlayersProjections(scraps, read_yaml("./config/score_settings.yml"))
+# 
+# # tipos de status que zera a pontuacao
+# # injuryStatus <- c("Suspended","Injured Reserve","Not With Team")
+# 
+# # cola informacao de times as projecoes de pontos dos jogadores
+# # projs.team <- projs %>% 
+# #   inner_join(mutate(player_ids, id=as.integer(id)), by="id") %>% 
+# #   addTeams(matchups, week, F) %>% 
+# #   ## Zera Estatisticas de quem estah machucado
+# #   mutate(
+# #     points  = ifelse(injuryGameStatus %in% injuryStatus, 0, points),
+# #     floor   = ifelse(injuryGameStatus %in% injuryStatus, 0, floor),
+# #     ceiling = ifelse(injuryGameStatus %in% injuryStatus, 0, ceiling),
+# #     sd_pts  = ifelse(injuryGameStatus %in% injuryStatus, 0, sd_pts),
+# #   )
+# 
+# ## projection report
+# # rmarkdown::render(
+# #   input = "./R/reports/ffa_players_projection.Rmd",
+# #   output_file = glue("../../{destPath}/reports/ffa_players_projection_week{week}.html"),
+# #   output_format = "flex_dashboard",
+# #   params = list(week=week)
+# # )
+# 
+# 
+# # calcula tabela de pontuacao para todos os jogadores usa na simulacao
+# source("./R/simulation/players_projections.R")
+# 
+# # simula as partidas
+# source(glue("./R/simulation/points_simulation_v{sim.version}.R"))
+# sim <- simulateGames(week)
+# 
+# # constroi o relatório
 # rmarkdown::render(
-#   input = "./R/reports/ffa_players_projection.Rmd",
-#   output_file = glue("../../{destPath}/reports/ffa_players_projection_week{week}.html"),
+#   input = glue("./R/reports/dudes_simulation_v{sim.version}.Rmd"),
+#   output_file = glue("../../{destPath}/reports/dudes_simulation_week{week}_{prefix}_v{sim.version}.html"),
 #   output_format = "flex_dashboard",
-#   params = list(week=week)
-# )
-
-
-# calcula tabela de pontuacao para todos os jogadores usa na simulacao
-source("./R/simulation/players_projections.R")
-
-# simula as partidas
-source(glue("./R/simulation/points_simulation_v{sim.version}.R"))
-sim <- simulateGames(week)
-
-# constroi o relatório
-rmarkdown::render(
-  input = glue("./R/reports/dudes_simulation_v{sim.version}.Rmd"),
-  output_file = glue("../../{destPath}/reports/dudes_simulation_week{week}_{prefix}_v{sim.version}.html"),
-  output_format = "flex_dashboard",
-    params = list(week=week)
-  )
-
-hist <- readRDS("./data/simulations_history.rds")
-sim %>% 
-  mutate(
-    week = week,
-    timestamp = now(),
-    prefix = prefix
-  ) %>% 
-  bind_rows(hist) %>%
-  saveRDS("./data/simulations_history.rds")
-
-
+#     params = list(week=week)
+#   )
+# 
+# hist <- readRDS("./data/simulations_history.rds")
+# sim %>% 
+#   mutate(
+#     week = week,
+#     timestamp = now(),
+#     prefix = prefix
+#   ) %>% 
+#   bind_rows(hist) %>%
+#   saveRDS("./data/simulations_history.rds")
+# 
+# 
--- R/simulation/points_simulation_v5.R
+++ R/simulation/points_simulation_v5.R
@@ -1,76 +1,114 @@
-.week     <- week
-.season   <- season
-.ptsproj  <- ptsproj
-.matchups <- matchups_games
-.teams    <- teams_rosters
-.plstats  <- players_stats
-.plids    <- player_ids
+library(tidyverse)
 
-# tipos de status que zera a pontuacao
-INJURY_STATUS_ZERO <- c("Suspended","Injured Reserve","Not With Team")
-
-mtch <- .matchups %>% 
-  select(matchupId, week, awayTeam.teamId, homeTeam.teamId)
-
-tms <- .teams %>% 
-  select(teamId, teamName=name, rosters) %>% 
-  unnest(rosters)
-
-stats <- .plstats %>% 
-  select(playerId, name, position, byeWeek, injuryGameStatus, weekPts) %>% 
-  unnest(weekPts) %>% 
-  filter(week==1) 
-
-projs <- .ptsproj %>% 
-  inner_join(select(.plids, id, playerId=nfl_id), by="id") %>% 
-  filter(week==.week, season==.season) %>% 
-  select(id, playerId, pts.proj) %>% 
-  group_by(id, playerId) %>% 
-  nest() %>% 
-  ungroup() %>% 
-  mutate(pts.proj = map(data, ~.x$pts.proj )) %>% 
-  select(-data)
-
-SIMULATION_SIZE = 1000
-
-players_sim <- tms %>% 
-  inner_join(projs, by="playerId") %>% 
-  mutate( simulation = map(pts.proj, sample, size=SIMULATION_SIZE, replace=T))
-
-teams_sim <- players_sim %>% 
-  inner_join(stats, by="playerId") %>% 
-  filter( rosterSlotId != 20 ) %>% 
-  select(teamId, playerId, simulation) %>% 
-  group_by(teamId) %>% 
-  nest() %>% 
-  ungroup() %>% 
-  mutate(simulation = map(data, function(.sim){
-    .sim %>% 
-      pull(simulation) %>% 
-      do.call(rbind, .) %>% 
-      colSums() %>% return()    
-  })) %>% 
-  select(-data)
-
-match_sim <- mtch %>% 
-  pivot_longer(c(-matchupId, -week), names_to="name", values_to="teamId") %>% 
-  mutate(name=str_remove(name, "\\.teamId")) %>% 
-  inner_join(teams_sim, by="teamId") %>% 
-  pivot_wider(id_cols = c(matchupId, week), names_from="name", values_from=c(teamId, simulation), names_sep=".")
-
-
-match_sim %>% 
-  mutate( win.homeTeam = map2(simulation.awayTeam, simulation.homeTeam, function(.sat, .sht) .sht>.sat) ) %>% 
-  mutate( win.awayTeam = map(win.homeTeam, function(.wht) !.wht ) ) %>% 
-  mutate( ptsdiff.homeTeam = map2(simulation.awayTeam, simulation.homeTeam, function(.sat, .sht) .sht-.sat) ) %>% 
-  mutate( 
-    winProb.homeTeam = map_dbl(win.homeTeam, mean), 
-    winProb.awayTeam = map_dbl(win.awayTeam, mean),
-    totalPts.homeTeam = map_dbl(simulation.homeTeam, median),
-    totalPts.awayTeam = map_dbl(simulation.awayTeam, median)
-  ) %>%
-  inner_join(select(.teams, teamId, name.homeTeam=name), by=c("teamId.homeTeam"="teamId")) %>% 
-  inner_join(select(.teams, teamId, name.awayTeam=name), by=c("teamId.awayTeam"="teamId")) %>% 
-  select(matchupId, name.awayTeam, totalPts.awayTeam, winProb.awayTeam,
-         winProb.homeTeam, totalPts.homeTeam, name.homeTeam) 
+simulateGames <- function(.week, .season, .ptsproj, .matchup_games, .teams_rosters, .players_stats, .players_id) {
   
+  # .week     <- week
+  # .season   <- season
+  # .ptsproj  <- ptsproj
+  # .matchups <- matchups_games
+  # .teams    <- teams_rosters
+  # .plstats  <- players_stats
+  # .plids    <- player_ids
+  
+  # so colunas de ids dos jogos
+  mtch <- .matchups %>% 
+    select(matchupId, week, awayTeam.teamId, homeTeam.teamId) %>% 
+    filter(week==.week)
+  
+  # unest o team roster
+  tms <- .teams %>% 
+    select(teamId, teamName=name, rosters) %>% 
+    unnest(rosters) %>% 
+    filter(week==.week)
+  
+  # filtra estatisticas dos  jogadores
+  stats <- .plstats %>% 
+    select(playerId, name, position, byeWeek, injuryGameStatus, weekPts) %>% 
+    unnest(weekPts) %>% 
+    filter(week==.week) 
+  
+  # estrutura de projecao dos jogadores
+  projs <- .ptsproj %>% 
+    inner_join(select(.plids, id, playerId=nfl_id), by="id") %>% 
+    filter(week==.week, season==.season) %>% 
+    select(id, playerId, pts.proj) %>% 
+    group_by(id, playerId) %>% 
+    nest() %>% 
+    ungroup() %>% 
+    mutate(pts.proj = map(data, ~.x$pts.proj )) %>% 
+    select(-data)
+  
+  # tamanho da simulacao
+  SIMULATION_SIZE = 1000
+  
+  # simulação dos jogadores
+  players_sim <- tms %>% 
+    inner_join(projs, by="playerId") %>% 
+    mutate( simulation = map(pts.proj, sample, size=SIMULATION_SIZE, replace=T))
+  
+  # pega os titulares e soma para obter a pontuacao dos jogos
+  teams_sim <- players_sim %>% 
+    # inner_join(stats, by="playerId") %>% 
+    filter( rosterSlotId != 20 ) %>%  # rosterId==20 indica banco
+    select(teamId, playerId, simulation) %>% 
+    group_by(teamId) %>% 
+    nest() %>% 
+    ungroup() %>% 
+    mutate(simulation = map(data, function(.sim){
+      # cada jogador tem um array de pontos projetados 
+      # extrai como uma lista (cada jogador uma linha)
+      # junta como uma matriz
+      # soma as colunas (cada coluna é uma simulação)
+      .sim %>% 
+        pull(simulation) %>% 
+        do.call(rbind, .) %>% 
+        colSums() %>%
+        return()    
+    })) %>% 
+    select(-data)
+  
+  # pivot para longo para fazer o bind da simulacao mais facil
+  matchup_sim <- mtch %>% 
+    pivot_longer(c(-matchupId, -week), names_to="name", values_to="teamId") %>% 
+    mutate(name=str_remove(name, "\\.teamId")) %>% 
+    inner_join(teams_sim, by="teamId") %>% 
+    # uma vez feito o join, pivot para wide de volta
+    pivot_wider(id_cols = c(matchupId, week), names_from="name", values_from=c(teamId, simulation), names_sep=".") %>% 
+    # prefico identificar a entidade
+    set_names(c("matchupId", "week", "awayTeam.teamId", "homeTeam.teamId", "awayTeam.simulation", "homeTeam.simulation")) %>% 
+    # cada time tem um array de pontos da simulacao dos titulares
+    mutate( 
+      # verifica pontuacao do home é maior que o visitante para saber se o home ganhou
+      homeTeam.win = map2(awayTeam.simulation, homeTeam.simulation, function(.sat, .sht) .sht>.sat), 
+      # se home ganhou visitante perdeu
+      awayTeam.win = map(homeTeam.win, function(.wht) !.wht ), 
+      # diferenca entre pontos (na direcao do home)
+      homeTeam.ptsdiff = map2(awayTeam.simulation, homeTeam.simulation, function(.sat, .sht) .sht-.sat),
+      # probabilidade de ganhar (media das vitorias)
+      homeTeam.winProb = map_dbl(homeTeam.win, mean), 
+      awayTeam.winProb = map_dbl(awayTeam.win, mean),
+      # pontos projetado (mediana da simulacao)
+      homeTeam.totalPts = map_dbl(homeTeam.simulation, median),
+      awayTeam.totalPts = map_dbl(awayTeam.simulation, median)
+    )
+  
+  # retorna a estrutura de calculo toda, mais facil para montar o report
+  list(
+    week     = week,
+    season   = season,
+    ptsproj  = ptsproj,
+    matchups = matchups_games,
+    teams    = teams_rosters,
+    plstats  = players_stats,
+    plids    = player_ids,
+    players_sim = players_sim,
+    teams_sim   = teams_sim,
+    matchup_sim = matchup_sim
+  )
+
+  
+}
+
+
+
+
