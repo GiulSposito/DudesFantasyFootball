62eaedb6395fb5fc4acfa5248018a81dc7ca0afb
Giuliano Sposito <gsposito@ciandt.com>
2020-09-14 09:59:14 -0300

    Finalizando código draft para incorporação das projeções do fantasy no código de scrapping do ffanalytics

--- R/_draft/fantasy_scrapping.R
+++ R/_draft/fantasy_scrapping.R
@@ -3,6 +3,31 @@ library(jsonlite)
 library(httr)
 library(ffanalytics)
 
+# parameters
+
+week <- 1
+season <- 2020
+
+source("./R/import/ffa_player_projection.R")
+scraps <- scrapPlayersPredictions(week, season, T)
+
+
+# carregando tabelas de "de para" de IDs de Jogadores
+load("../ffanalytics/R/sysdata.rda") # <<- Players IDs !!!
+miss_ids <- readRDS("./data/playerIds_not_mapped.rds") %>% 
+  mutate( 
+    id = as.character(id), 
+    nfl_id = as.character(nfl_id)
+  )
+
+player_ids <- player_ids %>%
+  # completa a tabela de mapeamento de projecoes do ffanalytics
+  bind_rows(miss_ids) %>% 
+  as_tibble()
+
+
+
+
 # Fantasy Projections #####
 
 ## Statistics IDs
@@ -45,6 +70,19 @@ fantasyProj <- stats %>%
   rename(playerId=id) %>%
   left_join(stat_ids, by=c("stat_id"="id"))
 
+# PLAYERS
+config <- yaml::read_yaml("./config/config.yml")
+source("./R/api/ffa_players.R")
+players_stats <- ffa_players_stats(config$authToken, config$leagueId, season, 1:week) %>% 
+  ffa_extractPlayersStats()
+
+nflplayers <- players_stats %>% 
+  select(src_id=playerId, player=name, position, team=nflTeamAbbr) %>% 
+  mutate(
+    src_id=as.character(src_id),
+    team=if_else(team=="",as.character(NA), team)
+  )
+
 ## Exploring a way to join
 
 ## NFL Proj Col Especification
@@ -54,39 +92,57 @@ ffa_columns = tibble(
   )
 
 
-# carregando tabelas de "de para" de IDs de Jogadores
-load("../ffanalytics/R/sysdata.rda") # <<- Players IDs !!!
-player_ids <- player_ids %>%
-  mutate(
-    id = as.integer(id), 
-    nfl_id = as.integer(nfl_id)) %>%
-  as_tibble() %>% 
-  # completa a tabela de mapeamento de projecoes do ffanalytics
-  bind_rows(readRDS("./data/playerIds_not_mapped.rds"))
-
-
 ## gerando uma tabela igual da FFA
-fantasyProj %>%
+nfl_scrap <- fantasyProj %>%
   inner_join(ffa_columns, by = "shortName") %>%
   mutate(value=as.numeric(value)) %>%
   select(src_id=playerId, colName, value) %>%
   filter(complete.cases(.)) %>%
   distinct() %>%
-  pivot_wider(id_cols=src_id, names_from=colName, values_from=value)
+  pivot_wider(id_cols=src_id, names_from=colName, values_from=value) %>% 
+  inner_join(select(player_ids, id, nfl_id), by=c("src_id"="nfl_id")) %>% 
+  inner_join(nflplayers, by="src_id") %>% 
+  mutate(
+    data_src="NFL",
+    week=as.character(week),
+    position = if_else(position=="DEF","DST",position)
+  ) %>% 
+  select(data_src, id, src_id, player, position, team, everything())
 
-player_ids %>% 
-  select(id, nfl_id)
   
 ## NFL Proj Col ID
-projection_sources[["NFL"]]$player_cols = c(src_id = "id", player = "name", team = "teamabbr", pos = "position")
+# player_cols = c(src_id = "id", player = "name", team = "teamabbr", pos = "position")
 
-  
-scrp <- ffanalytics::scrape_data(src="CBS",season = 2020, week = 1, pos = c("QB","RB","WR"))
+# scrp <- scrape_data(src="CBS",season = 2020, week = 1, pos = c("QB","RB","WR"))
+# scrp <- readRDS("./data/week1_scrap.rds")
+# 
+# scrp[[1]] <- scrp[[1]] %>% 
+#   bind_rows(filter(nfl_scrap, position=="QB"))
+# 
+# scrp[[1]] %>% View()
+# projections_table(scrp,  yaml::read_yaml("./config/score_settings.yml"))
 
-scrp[[1]] %>% names()
-scrp[[2]] %>% names()
-scrp[[3]] %>% names()
+scraps
 
-projection_sources[["NFL"]]
+names(scraps) %>% 
+  map(function(pos, .scrp=scraps, .nfl=nfl_scrap){
+    pos_col <- intersect(names(.scrp[[pos]]),names(nfl_scrap))
+    nfl_scrap %>% 
+      filter(position==pos) %>% 
+      select(pos_col) %>% 
+      bind_rows(.scrp[[pos]])
+  }) %>% 
+  set_names(names(scraps)) -> chg_scraps
 
-ff_player_data
+attr(chg_scraps, "season") <- attr(scraps,"season")
+attr(chg_scraps, "week") <- attr(scraps,"week")
+
+
+projections_table(chg_scraps,  yaml::read_yaml("./config/score_settings.yml")) %>% 
+  arrange(desc(points))
+
+projections_table(scraps,  yaml::read_yaml("./config/score_settings.yml")) %>% 
+  arrange(desc(points))
+
+scraps[[1]]$week
+chg_scraps[[1]] %>% names()
