9f78035701d6a124226e19445646a1a9fa797f5f
Giuliano Sposito <gsposito@ciandt.com>
2020-09-16 07:27:21 -0300

    Codigo para gerar histórico do ranking semanal

---
+++ R/_draft/week_rank_extract.R
@@ -0,0 +1,11 @@
+teams_rosters %>% 
+  select(teamId, name, imageUrl, week.stats, season.stats) %>% 
+  unnest(week.stats) %>% 
+  rename(week.pts = pts ) %>% 
+  unnest(season.stats) %>% 
+  mutate(
+    across(c(rank, divisionRank, wins, losses, ties, waiverPriority), as.integer),
+    across(c(pts, ptsAgainst), as.numeric)
+  ) %>% 
+  rename(season.pts=pts, season.ptsAgainst=ptsAgainst) %>% 
+  saveRDS("./data/rank_week01.rds")
