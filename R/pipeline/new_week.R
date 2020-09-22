b2d2dfafc0b274d29fca971f441068ac3a1a24f0
Giuliano Sposito <gsposito@ciandt.com>
2020-09-22 19:51:30 -0300

    Nova semana

--- R/pipeline/new_week.R
+++ R/pipeline/new_week.R
@@ -6,10 +6,10 @@ library(flexdashboard)
 library(yaml)
 
 # EXECUTION PARAMETERS ####
-week <- 2
+week <- 3
 season <- 2020
 config <- read_yaml("./config/config.yml")
-prefix <- "preSNF"
+prefix <- "preWaiver"
 destPath <- "static/reports/2020"
 sim.version <- 5
 
