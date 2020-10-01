9e8a1099ba52f99740c4fa6189fd2c01d2ca2de8
Giuliano Sposito <gsposito@ciandt.com>
2020-09-30 21:44:15 -0300

    nova versão do gamecenter em formato de tabela

--- R/_draft/game_center_plot.R
+++ R/_draft/game_center_plot.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(glue)
 
-sim <- readRDS("./data/simulation_v5_week3_preSNF.rds")
-.week <- 2
+.week <- 3
+.prefix <- "preSNF"
+
+sim <- readRDS(glue("./data/simulation_v5_week{.week}_{.prefix}.rds"))
 
 # game summary in long form
 teams_sims <- bind_rows(
@@ -28,76 +30,61 @@ teams_stats <- sim$teams %>%
     points = sum(weekPts, na.rm = T)
   )
 
-teams_sims %>% 
+teams <- sim$teams %>% 
+  select(teamId, name, imageUrl, rank) %>% 
+  inner_join(teams_sims, by = "teamId") %>% 
   inner_join(teams_stats, by = "teamId")
 
 
-team_points <- sim$teams %>% 
-  unnest(week.stats) %>% 
-  filter(week==.week) %>% 
-  select(teamId, name, pts, imageUrl)
-
 games_summary <- sim$matchup_sim %>% 
-  inner_join(set_names(team_points, paste0("away.",names(team_points))), by=c("awayTeam.teamId"="away.teamId")) %>% 
-  inner_join(set_names(team_points, paste0("home.",names(team_points))), by=c("homeTeam.teamId"="home.teamId")) %>% 
-  mutate(game=paste0(away.name, " @ ", home.name)) %>%
-  select(game, homeTeam.ptsdiff, homeTeam.ptsdiff.org, home.pts, away.pts,
-         home.teamId=homeTeam.teamId, away.teamId=awayTeam.teamId,
-         home.name,away.name, home.projPts=homeTeam.totalPts, away.projPts = awayTeam.totalPts) %>% 
+  inner_join(set_names(teams, paste0("away.",names(teams))), by=c("awayTeam.teamId"="away.teamId")) %>% 
+  inner_join(set_names(teams, paste0("home.",names(teams))), by=c("homeTeam.teamId"="home.teamId")) %>% 
+  mutate(game=paste0(away.name, " @ ", home.name)) %>% 
   mutate( away.nickname = gsub("([a-zA-Z\']+ )?", "", away.name),
           home.nickname = gsub("([a-zA-Z\']+ )?", "", home.name)) %>% 
   mutate( game.nickname = paste0(away.nickname, " @ ", home.nickname))
 
-team_prob <- sim_summary %>%
-  select(teamId, teamName=name, winProb, imageUrl)
+table_html_template <- "<table><thead><tr><th>{away.img}</th><th>{away.nickname}</th><th>{away.points}</th><th>@</th><th>{home.points}</th><th>{home.nickname}</th><th>{home.img}</th></tr></thead><tbody><tr><td>{away.winProb}</td><td>{away.remaining}</td><td>{away.pred.Pts}</td><td></td><td>{home.pred.Pts}</td><td>{home.remaining}</td><td>{home.winProb}</td></tr></tbody></table>"
+table_htmlcss_template <- "<table style='border-collapse:collapse;border-spacing:0' class='tg'><thead><tr><th style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:15px;font-weight:normal;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'>{away.img}</th><th style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:15px;font-weight:normal;overflow:hidden;padding:10px 5px;text-align:right;vertical-align:top;word-break:normal'>{away.nickname}</th><th style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:15px;font-weight:normal;overflow:hidden;padding:10px 5px;text-align:right;vertical-align:top;word-break:normal'><span style='font-weight:bold'>{away.points}</span></th><th style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:15px;font-weight:normal;overflow:hidden;padding:10px 5px;text-align:center;vertical-align:top;word-break:normal'>@</th><th style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:15px;font-weight:normal;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'><span style='font-weight:bold'>{home.points}</span></th><th style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:15px;font-weight:normal;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'>{home.nickname}</th><th style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:15px;font-weight:normal;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'>{home.img}</th></tr></thead><tbody><tr><td style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:12px;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'>{away.winProb}</td><td style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:12px;overflow:hidden;padding:10px 5px;text-align:right;vertical-align:top;word-break:normal'>{away.remaining}</td><td style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:12px;overflow:hidden;padding:10px 5px;text-align:right;vertical-align:top;word-break:normal'>{away.pred.Pts}</td><td style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:14px;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'></td><td style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:12px;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'>{home.pred.Pts}</td><td style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:12px;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'>{home.remaining}</td><td style='border-color:inherit;border-style:solid;border-width:1px;font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif !important;;font-size:12px;overflow:hidden;padding:10px 5px;text-align:left;vertical-align:top;word-break:normal'>{home.winProb}</td></tr></tbody></table>"
 
-team_rem <- sim$teams %>% 
-  select(teamId, rosters, season.stats) %>% 
-  unnest(c(rosters, season.stats)) %>% 
-  filter(rosterSlotId < 20) %>% 
-  group_by(teamId, record) %>% 
-  summarise(teamRemaining=sum(isEditable), .groups="keep") %>% 
-  ungroup() %>% 
-  mutate(record = str_remove(record, "-0")) %>% 
-  select(teamId, record, teamRemaining) %>% 
-  inner_join(team_prob, by="teamId")
+game_center <- games_summary %>%
+  select(away.imageUrl, away.nickname, away.points, home.points, home.nickname, home.imageUrl,
+         away.winProb, away.played, away.players, away.pred.Pts, 
+         home.winProb, home.played, home.players, home.pred.Pts) %>% 
+  mutate(
+    across(c(away.points, home.points, away.pred.Pts, home.pred.Pts), round, digits=1),
+    across(c(away.winProb, home.winProb), function(.x){ paste0(round(100*.x),"%") }),
+    away.img = glue("<img src='{away.imageUrl}' style='width:40px;height:40px' alt=''>"),
+    home.img = glue("<img src='{home.imageUrl}' style='width:40px;height:40px' alt=''>"),
+    away.remaining = glue("({away.played}/{away.players})"),
+    home.remaining = glue("({home.played}/{home.players})")
+  ) %>% 
+  mutate(
+    htmlTable = glue(table_html_template)
+  )
+  
 
-game_center <- games_summary %>% 
-  select(game.nickname, away.nickname, away.projPts, away.pts, away.teamId, away.name,
-         home.pts, home.projPts, home.nickname, home.teamId, home.name) %>% 
-  left_join(set_names(team_rem, paste0("home.",names(team_rem))), by = "home.teamId") %>% 
-  left_join(set_names(team_rem, paste0("away.",names(team_rem))), by = "away.teamId")
+game_center$htmlTable %>% write("./export/gamecenter.html")
 
-game_center %>% 
-    mutate(game.title=paste0("[", away.record,"] ", away.name, " @ ", home.name, " [", home.record, "]")) %>% 
-    ggplot(aes(x=0)) +
-    # projecao
-    geom_col(aes(y=home.projPts), fill="grey", width=0.07) +
-    geom_point(aes(y=home.projPts), color="grey", size=5) +
-    geom_text(aes(y=home.projPts, label=round(home.projPts, 1)), color="black", vjust=.4, nudge_y = 25, fontface="bold") +
-    geom_col(aes(y=-away.projPts), fill="grey", width=0.07) +
-    geom_point(aes(y=-away.projPts), color="grey", size=5) +
-    geom_text(aes(y=-away.projPts, label=round(away.projPts, 1)), color="black", vjust=.4, nudge_y = -25, fontface="bold") +
-    # pontuacao
-    geom_col(aes(y=home.pts), fill="red", width=0.07) +
-    geom_point(aes(y=home.pts), color="red", size=5) +
-    geom_text(aes(y=home.pts, label=round(home.pts, 1)), color="red", vjust=-.6, nudge_y = 14, fontface="bold") +
-    geom_text(aes(y=home.pts, label=paste0("(",9-home.teamRemaining,"/9)")), color="red", vjust=2, nudge_y = 10, size=3 ) +
-    geom_col(aes(y=-away.pts), fill="blue", width=0.07) +
-    geom_point(aes(y=-away.pts), color="blue", size=5) +
-    geom_text(aes(y=-away.pts, label=round(away.pts, 1)), color="blue", vjust=-.6, nudge_y = -14, fontface="bold") +
-    geom_text(aes(y=-away.pts, label=paste0("(",9-away.teamRemaining,"/9)")), color="blue", vjust=2, nudge_y = -10, size=3 ) +
-    # decorators
-    geom_text(aes(y=0, label=game.title), vjust=-3, fontface="bold") +
-    # facet
-    coord_flip() +
-    facet_wrap(~game.nickname, ncol = 1) +
-    labs(x="", title=glue("Last Update: XXX - XXXX"),
-         subtitle = "") +
-    xlim(-.5,.5) +
-    ylim(-1.5*max(game_center$away.projPts), 1.5*max(game_center$home.projPts)) +
-    theme_void() +
-    theme( strip.text = element_blank(), 
-           plot.margin = unit(c(5,5,5,5),"pt"),
-           plot.title = element_text(hjust = .5, size=10),
-           plot.subtitle = element_text(hjust = .5))
+-# 
+# team_prob <- sim_summary %>%
+#   select(teamId, teamName=name, winProb, imageUrl)
+# 
+# team_rem <- sim$teams %>% 
+#   select(teamId, rosters, season.stats) %>% 
+#   unnest(c(rosters, season.stats)) %>% 
+#   filter(rosterSlotId < 20) %>% 
+#   group_by(teamId, record) %>% 
+#   summarise(teamRemaining=sum(isEditable), .groups="keep") %>% 
+#   ungroup() %>% 
+#   mutate(record = str_remove(record, "-0")) %>% 
+#   select(teamId, record, teamRemaining) %>% 
+#   inner_join(team_prob, by="teamId")
+# 
+# game_center <- games_summary %>% 
+#   select(game.nickname, away.nickname, away.projPts, away.pts, away.teamId, away.name,
+#          home.pts, home.projPts, home.nickname, home.teamId, home.name) %>% 
+#   left_join(set_names(team_rem, paste0("home.",names(team_rem))), by = "home.teamId") %>% 
+#   left_join(set_names(team_rem, paste0("away.",names(team_rem))), by = "away.teamId")
+# 
+# game_center 
