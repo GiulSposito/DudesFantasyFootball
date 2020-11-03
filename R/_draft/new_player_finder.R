7b5eb1f8d5de8bd7038f3999dec246258c783e15
Giuliano Sposito <gsposito@ciandt.com>
2020-11-03 08:42:48 -0300

    Codigo que sugere escalação e waivers

--- R/_draft/new_player_finder.R
+++ R/_draft/new_player_finder.R
@@ -10,28 +10,36 @@ pHist <- sim$players_stats %>%
   unnest(weekPts) %>% 
   select(playerId, name, position, byeWeek, week, weekPts) %>% 
   filter(byeWeek!=week, week<.week) %>% 
-  mutate( weekPts = if_else(is.na(weekPts), 0, weekPts) ) %>% 
+  # mutate( weekPts = if_else(is.na(weekPts), 0, weekPts) ) %>%
+  filter(complete.cases(.)) %>% 
   group_by(playerId, name, position) %>% 
   summarise(
     avgPts = mean(weekPts),
-    sdPts  = sd(weekPts)
+    sdPts  = sd(weekPts),
+    games  = n()
   ) %>% 
+  ungroup() %>% 
   arrange(desc(avgPts))
 
 pProj<- sim$proj_table %>% 
   inner_join(select(sim$players_id, id, playerId=nfl_id), by="id") %>% 
   select(17,10:16)
 
+rosters <- sim$teams %>% 
+  unnest(rosters) %>% 
+  select(teamId, team=name, playerId)
 
 pHist %>% 
-  inner_join(pProj) %>% 
+  left_join(rosters, by="playerId") %>% 
+  left_join(pProj, by="playerId") %>% 
+  mutate(team=if_else(is.na(team),"FA", team)) %>% 
+  filter(is.na(teamId)|teamId==4) %>% 
   filter(position=="WR") %>% 
-  ungroup() %>% 
   top_n(n = 50, wt=avgPts) %>% 
-  ggplot(aes(points, avgPts)) +
+  ggplot(aes(avgPts, sdPts)) +
   geom_abline(slope=1) +
-  geom_point() +
+  geom_point(aes(color=team, size=games)) +
   geom_text_repel(aes(label=name)) +
-  labs(x="projecao",y="pontuacao") +
+  labs(x="sd",y="mean") +
   theme_minimal()
-
+ 
