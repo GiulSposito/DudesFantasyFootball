2d8615f1800d8cbb0ced51c36a300e7568972d97
Giuliano Sposito <gsposito@ciandt.com>
2020-11-03 08:39:36 -0300

    testando gráfico de best coaches

--- R/_draft/best_coach_changes.R
+++ R/_draft/best_coach_changes.R
@@ -4,8 +4,8 @@ library(glue)
 w1 = 2
 w2 = 3
 
-sim1 <- readRDS(glue("./data/simulation_v5_week{w1}_posMNF.rds"))
-sim2 <- readRDS(glue("./data/simulation_v5_week{w2}_posMNF.rds"))
+sim1 <- readRDS(glue("./data/simulation_v5_week{w1}_final.rds"))
+sim2 <- readRDS(glue("./data/simulation_v5_week{w2}_final.rds"))
 
 team1 <- sim1$teams %>% 
   unnest(rosters) %>% 
@@ -28,6 +28,7 @@ contratados <- team2 %>%
 
 changes <- bind_rows(liberados, contratados)
 
+# saldo
 sim2$players_stats %>% 
   unnest(weekPts) %>% 
   filter(week==w2) %>% 
@@ -35,11 +36,12 @@ sim2$players_stats %>%
   inner_join(changes) %>% 
   mutate( weekPts = if_else(status=="contratado", weekPts, -weekPts)) %>% 
   group_by(teamId) %>% 
-  summarise( saldo = sum(weekPts, na.rm = T)) %>% 
+  summarise( balance = sum(weekPts, na.rm = T)) %>% 
   inner_join(select(sim1$teams, teamId, name)) %>% 
-  select(teamId, name, saldo) %>% 
-  arrange(desc(saldo))
-  
+  select(teamId, name, balance) %>% 
+  arrange(desc(balance))
+
+# changes  
 sim2$players_stats %>% 
   unnest(weekPts) %>% 
   filter(week==w2) %>% 
