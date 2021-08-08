123e92a64aa34fab960186e98b6f9576ece71cf3
Giuliano Sposito <gsposito@ciandt.com>
2021-08-08 13:45:07 -0300

    very old update

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -15,7 +15,7 @@ library(yaml)
 options(dplyr.summarise.inform = FALSE)
 
 # EXECUTION PARAMETERS ####
-week <- 14
+week <- 16
 season <- 2020
 config <- read_yaml("./config/config.yml")
 prefix <- "final"
