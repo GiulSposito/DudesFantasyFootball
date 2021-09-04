523c0d2bc769bcc43b6fbc927f5ca4fa6ee16b64
Giuliano Sposito <gsposito@ciandt.com>
2021-09-04 10:17:17 -0300

    Codigo para scrapear a temporada toda simulador de draft picks

--- R/_draft/draft_pick_simulator.R
+++ R/_draft/draft_pick_simulator.R
@@ -1,40 +1,49 @@
+library(tidyverse)
+
+season_proj <- readRDS("./data/season_projtable.rds")
+
 players <- season_proj %>% 
   mutate(playerId = as.integer(id)) %>% 
   relocate(playerId, everything()) %>% 
   filter(avg_type=="average")
 
+
+season_proj %>% 
+  filter(avg_type=="average", position=="TE") %>% 
+  arrange(desc(ceiling)) %>% 
+  View("bench")
+
 # 10 times + 1 QB + 2 WR + 2 RB + 1 TE + 1 K + 1 DST + 1 W/R
 # 10 * (1+2+2+1+1+1)
 
 rosters <- tibble(
-  teamId = 1:10,
+  teamId = 1:14,
   slots = list(
     tibble(
-        slotId   = 1:8,
-        position = c("QB","WR","WR","RB","RB","TE","K","DST"),
+        slotId   = 1:9,
+        position = c("QB","WR","WR","RB","RB","TE","K","DST","W/R"),
         playerId = as.integer(NA)
       )
     )
   ) %>% unnest(slots)
 
 # snake 3RR
-picks <- tibble(
-  pick = 1:80,
-  teamId = c(1:10,10:1,10:1,rep(c(1:10,10:1),2), 1:10),
-  playerId = as.integer(NA)
-)
-
+# picks <- tibble(
+#   pick = 1:90,
+#   teamId = c(1:10,10:1,10:1,rep(c(1:10,10:1),3)),
+#   playerId = as.integer(NA)
+# )
 
 # snake 
 picks <- tibble(
-  pick = 1:80,
-  teamId = rep(c(1:10,10:1),4),
+  pick = 1:126,
+  teamId = c(rep(c(1:14,14:1),4),1:14),
   playerId = as.integer(NA)
 )
 
 
 # para todo o pick
-for(p in 1:80){
+for(p in 1:126){
 
   # time da vez
   pteam <- picks[p,]$teamId
@@ -44,6 +53,20 @@ for(p in 1:80){
     filter(teamId==pteam, is.na(playerId)) %>% 
     select(-playerId)
   
+  # trata a posicao coringa
+  if (9 %in% avail_slots$slotId) {
+    
+    avail_slots <- tibble(
+      teamId=c(pteam,pteam),
+      slotId=c(9,9),
+      position=c("WR","RB")
+    ) %>% 
+      bind_rows(avail_slots) %>% 
+      filter(position!="W/R") %>% 
+      arrange(slotId)
+    
+  }
+  
   # jogadores disponiveis
   avail_players <- players %>% 
     anti_join(rosters, by="playerId") %>% 
@@ -52,7 +75,7 @@ for(p in 1:80){
   # melhor pick
   best_pick <- avail_players %>% 
     inner_join(avail_slots, by="position") %>% 
-    arrange(desc(points_vor)) %>% 
+    arrange(desc(floor_vor), slotId) %>% 
     head(1) %>%
     select(teamId, slotId, pickPos=position, pickId = playerId)
     
@@ -69,15 +92,35 @@ for(p in 1:80){
 
 rosters %>% 
   inner_join(select(picks, playerId, pick), by="playerId") %>% 
-  inner_join(select(players, playerId, first_name, last_name, team, floor,points,ceiling), by="playerId") %>% 
+  inner_join(select(players, playerId, first_name, last_name, pos=position, team, floor,points,ceiling), by="playerId") %>% 
   group_by(teamId) %>% 
   summarise(
     floor=sum(floor),
     points=sum(points),
     ceiling=sum(ceiling)
   ) %>% 
+  ungroup() %>% 
+  arrange(desc(points))
   ggplot(aes(x=as.factor(teamId),y=points)) +
   geom_point(size=2) +
-  geom_errorbar(aes(ymin=floor, ymax=ceiling)) +
+  geom_errorbar(aes(ymin=floor, ymax=ceiling), width=.3) +
   ylim(0,NA) +
+  labs( title="Projeção de Pontos Temporada", 
+        subtitle="Full PPR | Snake Draft | VOR",
+        x="Team Draft Position",
+        y="Fantasy Points") +
+  theme_minimal()
+
+
+rosters %>% 
+  inner_join(select(picks, playerId, pick), by="playerId") %>% 
+  inner_join(select(players, playerId, first_name, last_name, pos=position, team, floor,points,ceiling), by="playerId") %>% 
+  mutate(teamId = as.factor(teamId)) %>% 
+  select(pick, teamId, pos, points) %>% 
+  ggplot(aes(x=pick, y=points, color=teamId, shape=pos)) +
+  geom_point() +
+  labs( title="Projeção de Pontos Temporada", 
+        subtitle="Full PPR | Snake Draft | VOR",
+        x="Draft Pick",
+        y="Fantasy Points") +
   theme_minimal()
--- R/_draft/season_scraps.R
+++ R/_draft/season_scraps.R
@@ -2,26 +2,20 @@ library(tidyverse)
 library(ffanalytics)
 
 # usual scrap
-scrp <- scrape_data(src = c("CBS", "ESPN", "FantasyData", "FantasyPros", "FantasySharks", "FFToday",
-                            "NumberFire", "Yahoo", "FantasyFootballNerd", "NFL", "RTSports",
+scrp <- scrape_data(src = c("ESPN", "FantasyData", "FantasyPros", "FantasySharks", "FFToday", # "CBS", 
+                            "NumberFire", "Yahoo", "FantasyFootballNerd", "NFL", "RTSports", # "FleaFlicker"
                             "Walterfootball", "FleaFlicker"),
                     pos = c("QB", "RB", "WR", "TE", "K", "DST"),
                     season = 2021,
                     week = 0)
 
-# saveRDS(scrp,"./data/season_scrap.rds")
-# saveRDS(scrp,"./data/season_scrap_fleaflicker.rds")
+
+scrp <- scrape_data(pos = c("QB", "RB", "WR", "TE", "K", "DST"),season = 2021,week = 0)
+
 
 # checking
 scrp %>% 
   map_df(~select(.x, data_src, team, id), .id="pos") %>% 
-  count(data_src, pos) %>% 
-  pivot_wider(names_from = "pos",values_from="n")
-
-
-scrp %>% 
-  map_df(~select(.x, data_src, team), .id="pos") %>% 
-  distinct() %>% 
   count(data_src, pos) %>% 
   pivot_wider(names_from = "pos",values_from="n")
 
@@ -65,3 +59,17 @@ season_proj %>%
   filter(avg_type=="average") %>% 
   arrange(desc(floor_vor)) %>% 
   View()
+
+season_proj_old <- readRDS("./data/season_projtable_20210816.rds")
+
+proj_old <- season_proj_old %>% 
+  select(id, avg_type, points_old = points)
+
+season_proj %>% 
+  inner_join(proj_old, by = c("id", "avg_type")) %>% 
+  filter( position=="DST" ) %>% 
+  mutate(points_diff = points - points_old) %>% 
+  filter(points_diff != 0,
+         avg_type == "average") %>% 
+  arrange(desc(points_diff)) %>% 
+  select(first_name, last_name, position, team, pts_lastweek= points_old, pts_now=points, diff=points_diff)
